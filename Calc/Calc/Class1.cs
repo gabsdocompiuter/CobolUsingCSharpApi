40569102f206496edb33a987b60ae343c7bf3266
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-01-17 15:56:59 -0200

    Calculator Project

---
+++ Calc/Calc/Class1.cs
@@ -0,0 +1,25 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public class Class1
+    {
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
+        public static void TwoStringParams(string a, string b)
+        {
+            MessageBox.Show($"a: {a}, b:{b}");
+        }
+
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
+        public static void TwoIntParams(int a, int b)
+        {
+            MessageBox.Show($"a: {a}, b:{b}");
+        }
+    }
+}
