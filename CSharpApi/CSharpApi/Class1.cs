3391c0af204feeba52b82227749410b52387d03d
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-01-17 14:01:08 -0200

    first commit of C# code
    Created method ShowMessage()

---
+++ CSharpApi/CSharpApi/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using RGiesecke.DllExport;
+
+namespace CSharpApi
+{
+    public class Class1
+    {
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
+        public static void ShowMessage()
+        {
+            MessageBox.Show("Hello!");
+        }
+    }
+}
